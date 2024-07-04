ed0de53dcdb83532cc8470a9d11a3e5791c0474d
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-04 19:50:13 +0300

    Add logger

--- internal/server/logging/logging.go
+++ internal/server/logging/logging.go
@@ -1,1 +1,23 @@
 package logging
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/kirillgashkov/assignment-timetrack/internal/server/config"
+)
+
+func NewLogger(cfg *config.Config) *slog.Logger {
+	var logger *slog.Logger
+
+	switch cfg.Mode {
+	case config.ModeDevelopment:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case config.ModeProduction:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		panic("invalid mode")
+	}
+
+	return logger
+}
