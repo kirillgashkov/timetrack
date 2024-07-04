bd1f22218fc9a096b0c621652703be513dc5cc48
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-04 19:54:58 +0300

    Add mainErr

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := mainErr(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func mainErr() error {
+	return nil
+}
