d338aaffef2e39091a9eb56fcb11134e57ebee56
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-06 00:35:32 +0300

    Add OpenAPI base

--- api/api.go
+++ api/api.go
@@ -1,3 +1,3 @@
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=oapi-codegen.config.yaml openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=timetrack/v1/oapi-codegen.config.yaml timetrack/v1/openapi.yaml
 
 package api
