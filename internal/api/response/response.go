9fb93104ad2dd88ab4c64dcf8be4a9afe369e522
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-07 00:37:17 +0300

    Add status code to JSON responses

--- internal/api/response/response.go
+++ internal/api/response/response.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 )
 
-func MustWriteJSON(w http.ResponseWriter, v interface{}) {
-	if err := WriteJSON(w, v); err != nil {
+func MustWriteJSON(w http.ResponseWriter, v any, code int) {
+	if err := WriteJSON(w, v, code); err != nil {
 		panic(err)
 	}
 }
 
-func WriteJSON(w http.ResponseWriter, v interface{}) error {
+func WriteJSON(w http.ResponseWriter, v any, code int) error {
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
 	return json.NewEncoder(w).Encode(v)
 }
