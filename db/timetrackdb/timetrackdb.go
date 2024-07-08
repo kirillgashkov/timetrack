b74e8d0da7bec55f6c874d9a5870464d134f59f2
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-09 00:44:05 +0300

    Add TaskUserStatus* consts

--- db/timetrackdb/timetrackdb.go
+++ db/timetrackdb/timetrackdb.go
@@ -5,3 +5,10 @@ const (
 	TrackTypeStart = "start"
 	TrackTypeStop  = "stop"
 )
+
+// TaskUserStatusActive and TaskUserStatusInactive are the statuses of a user's
+// current participation in a task.
+const (
+	TaskUserStatusActive   = "active"
+	TaskUserStatusInactive = "inactive"
+)
