dcca4b80f1c6c196dec46614d9dbe328df70de44
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-08 19:46:52 +0300

    Add the rest of task CRUD

--- internal/task/task.go
+++ internal/task/task.go
@@ -42,7 +42,8 @@ func NewService(db *pgxpool.Pool) *Service {
 
 func (s *Service) Create(ctx context.Context, create *Create) (*Task, error) {
 	rows, err := s.db.Query(
-		ctx, `INSERT INTO tasks (description) VALUES ($1) RETURNING id, description`,
+		ctx,
+		`INSERT INTO tasks (description) VALUES ($1) RETURNING id, description`,
 		create.Description,
 	)
 	if err != nil {
@@ -55,6 +56,70 @@ func (s *Service) Create(ctx context.Context, create *Create) (*Task, error) {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) && pgerrcode.IsIntegrityConstraintViolation(pgErr.Code) {
 			return nil, ErrAlreadyExists
+		}
+		return nil, errors.Join(errors.New("failed to collect task"), err)
+	}
+	return &task, nil
+}
+
+func (s *Service) Get(ctx context.Context, id int) (*Task, error) {
+	rows, err := s.db.Query(
+		ctx,
+		`SELECT id, description FROM tasks WHERE id = $1`,
+		id,
+	)
+	if err != nil {
+		return nil, errors.Join(errors.New("failed to select task"), err)
+	}
+	defer rows.Close()
+
+	task, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[Task])
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, errors.Join(errors.New("failed to collect task"), err)
+	}
+	return &task, nil
+}
+
+func (s *Service) Update(ctx context.Context, id int, update *Update) (*Task, error) {
+	rows, err := s.db.Query(
+		ctx,
+		`UPDATE tasks SET description = coalesce($1, description) WHERE id = $2 RETURNING id, description`,
+		update.Description,
+		id,
+	)
+	if err != nil {
+		return nil, errors.Join(errors.New("failed to update task"), err)
+	}
+	defer rows.Close()
+
+	task, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[Task])
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, errors.Join(errors.New("failed to collect task"), err)
+	}
+	return &task, nil
+}
+
+func (s *Service) Delete(ctx context.Context, id int) (*Task, error) {
+	rows, err := s.db.Query(
+		ctx,
+		`DELETE FROM tasks WHERE id = $1 RETURNING id, description`,
+		id,
+	)
+	if err != nil {
+		return nil, errors.Join(errors.New("failed to delete task"), err)
+	}
+	defer rows.Close()
+
+	task, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[Task])
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNotFound
 		}
 		return nil, errors.Join(errors.New("failed to collect task"), err)
 	}
