5ed78debddbafb546ad9d38782ac48a92286d171
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-10 01:26:53 +0300

    Implement task start and stop handlers

--- internal/tracking/handler.go
+++ internal/tracking/handler.go
@@ -1,6 +1,12 @@
 package tracking
 
-import "net/http"
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/kirillgashkov/timetrack/internal/app/api/apiutil"
+)
 
 type Handler struct {
 	Service *Service
@@ -10,12 +16,38 @@ type Handler struct {
 //
 //nolint:revive
 func (h *Handler) PostTasksIdStart(w http.ResponseWriter, r *http.Request, id int) {
-	panic("implement me")
+	userID := 1 // TODO: use real user ID
+
+	err := h.Service.StartTask(r.Context(), TaskID(id), UserID(userID))
+	if err != nil {
+		if errors.Is(err, ErrAlreadyStarted) {
+			apiutil.MustWriteError(w, "task already started", http.StatusBadRequest)
+			return
+		}
+		slog.Error("failed to start task", "error", err)
+		apiutil.MustWriteInternalServerError(w)
+		return
+	}
+
+	apiutil.MustWriteNoContent(w)
 }
 
 // PostTasksIdStop handles "POST /tasks/{id}/stop".
 //
 //nolint:revive
 func (h *Handler) PostTasksIdStop(w http.ResponseWriter, r *http.Request, id int) {
-	panic("implement me")
+	userID := 1 // TODO: use real user ID
+
+	err := h.Service.StopTask(r.Context(), TaskID(id), UserID(userID))
+	if err != nil {
+		if errors.Is(err, ErrNotStarted) {
+			apiutil.MustWriteError(w, "task not started", http.StatusBadRequest)
+			return
+		}
+		slog.Error("failed to stop task", "error", err)
+		apiutil.MustWriteInternalServerError(w)
+		return
+	}
+
+	apiutil.MustWriteNoContent(w)
 }
