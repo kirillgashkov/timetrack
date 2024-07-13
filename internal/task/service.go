540302af42aeb02e50490fd710ae5d0f6ec5b788
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-13 19:41:55 +0300

    Simplify task service

--- internal/task/service.go
+++ internal/task/service.go
@@ -42,51 +42,32 @@ func NewServiceImpl(db *pgxpool.Pool) *ServiceImpl {
 }
 
 func (s *ServiceImpl) Create(ctx context.Context, create *CreateTask) (*Task, error) {
-	rows, err := s.db.Query(
-		ctx,
-		`INSERT INTO tasks (description) VALUES ($1) RETURNING id, description`,
-		create.Description,
-	)
-	if err != nil {
-		return nil, errors.Join(errors.New("failed to insert task"), err)
-	}
-	defer rows.Close()
-
-	task, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[Task])
-	if err != nil {
-		return nil, errors.Join(errors.New("failed to collect task"), err)
-	}
-	return &task, nil
+	q := `INSERT INTO tasks (description) VALUES ($1) RETURNING id, description`
+	return s.queryOne(ctx, q, create.Description)
 }
 
 func (s *ServiceImpl) Get(ctx context.Context, id int) (*Task, error) {
-	rows, err := s.db.Query(
-		ctx,
-		`SELECT id, description FROM tasks WHERE id = $1`,
-		id,
-	)
-	if err != nil {
-		return nil, errors.Join(errors.New("failed to select task"), err)
-	}
-	defer rows.Close()
-
-	task, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[Task])
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, ErrNotFound
-		}
-		return nil, errors.Join(errors.New("failed to collect task"), err)
-	}
-	return &task, nil
+	q := `SELECT id, description FROM tasks WHERE id = $1`
+	return s.queryOne(ctx, q, id)
 }
 
 func (s *ServiceImpl) List(ctx context.Context, offset, limit int) ([]Task, error) {
-	rows, err := s.db.Query(
-		ctx,
-		`SELECT id, description FROM tasks ORDER BY id OFFSET $1 LIMIT $2`,
-		offset,
-		limit,
-	)
+	q := `SELECT id, description FROM tasks ORDER BY id OFFSET $1 LIMIT $2`
+	return s.queryAll(ctx, q, offset, limit)
+}
+
+func (s *ServiceImpl) Update(ctx context.Context, id int, update *UpdateTask) (*Task, error) {
+	q := `UPDATE tasks SET description = coalesce($1, description) WHERE id = $2 RETURNING id, description`
+	return s.queryOne(ctx, q, update.Description, id)
+}
+
+func (s *ServiceImpl) Delete(ctx context.Context, id int) (*Task, error) {
+	q := `DELETE FROM tasks WHERE id = $1 RETURNING id, description`
+	return s.queryOne(ctx, q, id)
+}
+
+func (s *ServiceImpl) queryAll(ctx context.Context, query string, args ...any) ([]Task, error) {
+	rows, err := s.db.Query(ctx, query, args...)
 	if err != nil {
 		return nil, errors.Join(errors.New("failed to select tasks"), err)
 	}
@@ -99,44 +80,15 @@ func (s *ServiceImpl) List(ctx context.Context, offset, limit int) ([]Task, erro
 	return tasks, nil
 }
 
-func (s *ServiceImpl) Update(ctx context.Context, id int, update *UpdateTask) (*Task, error) {
-	rows, err := s.db.Query(
-		ctx,
-		`UPDATE tasks SET description = coalesce($1, description) WHERE id = $2 RETURNING id, description`,
-		update.Description,
-		id,
-	)
+func (s *ServiceImpl) queryOne(ctx context.Context, query string, args ...any) (*Task, error) {
+	rows, err := s.db.Query(ctx, query, args...)
 	if err != nil {
-		return nil, errors.Join(errors.New("failed to update task"), err)
+		return nil, errors.Join(errors.New("failed to select task"), err)
 	}
 	defer rows.Close()
 
 	task, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[Task])
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, ErrNotFound
-		}
-		return nil, errors.Join(errors.New("failed to collect task"), err)
-	}
-	return &task, nil
-}
-
-func (s *ServiceImpl) Delete(ctx context.Context, id int) (*Task, error) {
-	rows, err := s.db.Query(
-		ctx,
-		`DELETE FROM tasks WHERE id = $1 RETURNING id, description`,
-		id,
-	)
-	if err != nil {
-		return nil, errors.Join(errors.New("failed to delete task"), err)
-	}
-	defer rows.Close()
-
-	task, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[Task])
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, ErrNotFound
-		}
 		return nil, errors.Join(errors.New("failed to collect task"), err)
 	}
 	return &task, nil
