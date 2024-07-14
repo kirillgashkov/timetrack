981ae3e16bfc10d0c9f028507fe9c58985332917
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-07-14 14:08:16 +0300

    Add not found error handling to task service

--- internal/task/service.go
+++ internal/task/service.go
@@ -3,6 +3,8 @@ package task
 import (
 	"context"
 	"errors"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -89,6 +91,10 @@ func (s *ServiceImpl) queryOne(ctx context.Context, query string, args ...any) (
 
 	task, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[Task])
 	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && pgerrcode.IsIntegrityConstraintViolation(pgErr.Code) {
+			return nil, ErrNotFound
+		}
 		return nil, errors.Join(errors.New("failed to collect task"), err)
 	}
 	return &task, nil
